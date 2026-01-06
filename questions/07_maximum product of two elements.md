2071ae538f105482ec273c215fb509b0f2e6dd67
Deep Mohuri <deepmohuri65@gmail.com>
2026-01-06 08:54:26 +0530

    7th question array mult

---
+++ questions/07_maximum product of two elements.md
@@ -0,0 +1,17 @@
+Problem
+
+Given an array nums, return the maximum product of two elements.
+
+Function signature
+def max_product(nums: list[int]) -> int:
+    ...
+
+Examples
+[3, 4, 5, 2] → 20
+[-10, -3, 5, 6] → 30   # (-10 * -3)
+[1, 2] → 2
+
+Hint (don’t ignore this)
+Negatives matter
+Think about two largest and two smallest
+Time must be O(n).
---
+++ solutions/07_maximum product of two elements.py
@@ -0,0 +1,20 @@
+def max_product(nums:list[int]) -> int:
+    # nums.sort()
+    
+    # return max(nums[-1] * nums[-2], nums[0]*nums[1])
+    max1 = max2 = float["-inf"]
+    min1 = min2 = float["inf"]
+
+    for num in nums:
+        if num > max1:
+            max2 = max1
+            max1 = num
+        elif num > max2:
+            max2 = num
+        if num < min1:
+            min2 = min1
+            min1 = num
+        elif num < min2:
+            min2 = num
+
+    return max(max1 * max2, min1 * min2)
