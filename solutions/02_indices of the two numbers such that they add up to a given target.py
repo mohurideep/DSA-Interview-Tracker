dbfedd5ad87c5aef16b458a7d25005300ce0abf0
Deep Mohuri <deepmohuri65@gmail.com>
2026-01-21 17:02:36 +0530

    added bruteforce for question 2

--- solutions/02_indices of the two numbers such that they add up to a given target.py
+++ solutions/02_indices of the two numbers such that they add up to a given target.py
@@ -6,3 +6,10 @@ def two_sum(nums:list[int], target:int) -> list[int]:
             return [i,lookup[complemet]]
         lookup[num]=i
     return []
+
+def two_sum(nums:list[int], target:int) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j] == target:
+                return [i,j]
+    return None
