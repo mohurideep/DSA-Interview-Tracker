f104faf25661554e48c6a0c37e66133991a0afda
Deep Mohuri <deepmohuri65@gmail.com>
2026-02-01 14:35:55 +0530

    Add brute-force O(n^3) solution for longest substring problem
    Introduced length_of_longest_substring1, an O(n^3) brute-force implementation for finding the longest substring without repeating characters. Added a test print statement for demonstration.

--- solutions/04_longest substring without repeating characters.py
+++ solutions/04_longest substring without repeating characters.py
@@ -12,3 +12,22 @@ def length_of_longest_substring(s:str) -> int:
         max_len = max(max_len, right-left+1)
 
     return max_len
+
+
+def length_of_longest_substring1(s: str) -> int:
+    # O(n^3) solution
+    if len(s) == 0:
+        return 0
+    max_count = 1
+    for i in range(len(s)):
+        count = 1
+        for j in range(i+1,len(s)):
+            if s[j] in s[i:j]:
+                break
+            count +=1
+        max_count = max(max_count,count)
+        
+    return max_count
+
+
+print (length_of_longest_substring1("pwwkew"))
