2f7bba710badf694909fcb6ab0c0b573dab4707c
Deep Mohuri <deepmohuri65@gmail.com>
2026-02-02 00:15:51 +0530

    Add brute-force solution for longest substring with K distinct chars
    Added the function longest_substring_k_distinct1, which implements a brute-force approach to find the longest substring with at most K distinct characters. Includes a test print statement for demonstration.

--- solutions/05_Longest substring with at most K distinct character.py
+++ solutions/05_Longest substring with at most K distinct character.py
@@ -17,3 +17,25 @@ def longest_substring_k_distinct(s:str, k:int) -> int:
         max_length=max(max_length,right-left+1)
 
     return max_length
+
+
+
+def longest_substring_k_distinct1(s: str, k: int) -> int:
+    if len(s) == 0 or k == 0:
+        return 0
+    max_length = 0
+    for i in range(len(s)):
+        unique = {}
+        for j in range(i, len(s)):
+            if s[j] in unique:
+                unique[s[j]] += 1
+            elif len(unique) < k:
+                unique[s[j]] = 1
+            else:
+                break
+            max_length = max(max_length, j - i + 1)
+        
+    return max_length
+
+
+print(longest_substring_k_distinct1("abaccc", 2))
