44d2b1a07ad6e84daa778592c4b810cf26a9a079
Deep Mohuri <deepmohuri65@gmail.com>
2026-03-04 19:29:01 +0530

    trying & Learning DP solution for question 24

--- solutions/24_Longest Valid Parentheses.py
+++ solutions/24_Longest Valid Parentheses.py
@@ -20,4 +20,20 @@ def is_valid(s: str) -> bool:
     return balance == 0
 
 
-print(longest_valid_parentheses("((())"))  # Output: 4
+# print(longest_valid_parentheses("((())"))  # Output: 4
+
+#time complexity o(n^2)
+
+def longest_valid_parentheses_dp(s: str) -> int:
+    max_length = 0
+    dp = [0] * len(s)
+    for i in range(1, len(s)):
+        if s[i] == ')':
+            if s[i - 1] == '(':
+                dp[i] = (dp[i - 2] if i >= 2 else 0) + 2
+            elif i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
+                dp[i] = dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] - 2 >= 0 else 0) + 2
+            max_length = max(max_length, dp[i])
+    return max_length
+
+
