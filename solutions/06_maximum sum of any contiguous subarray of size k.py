a92dfe11b241530a998d9df897baeea1cda6ccfd
Deep Mohuri <deepmohuri65@gmail.com>
2026-03-12 22:01:18 +0530

    added slicing approach for the question 06

--- solutions/06_maximum sum of any contiguous subarray of size k.py
+++ solutions/06_maximum sum of any contiguous subarray of size k.py
@@ -7,3 +7,16 @@ def max_sum_subarray(nums: list[int], k: int) -> int:
         max_sum = max(max_sum, window_sum)
 
     return max_sum
+
+print(max_sum_subarray([2, 1, 5, 1, 3, 2], 3))  # Output: 9
+
+# using Slicing
+def max_sum_subarray_slicing(nums: list[int], k: int) -> int:
+    max_sum = 0
+    for i in range(len(nums) - k + 1):
+        max_sum = max(max_sum, sum(nums[i:i + k]))
+    return max_sum
+
+    # return max(sum(nums[i:i + k]) for i in range(len(nums) - k + 1))
+
+print(max_sum_subarray_slicing([2, 1, 5, 1, 3, 2], 3))  # Output: 9
